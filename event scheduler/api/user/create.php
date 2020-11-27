be2c063db9460ce7f4eebf81dd23f8f9d7cff548
ggomez31 <37435909+ggomez31@users.noreply.github.com>
2020-11-27 17:30:20 -0500

    Add files via upload

---
+++ event scheduler/api/user/create.php
@@ -0,0 +1,43 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+include('..\class\User.php');
+//$user = new Users();
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+switch($requestMethod) {
+	case 'POST':
+		$user = new Users($data->username, $data->password);
+
+		$userInfo = $user->getUser();
+
+		if (!empty($userInfo))
+		{
+			header("HTTP/1.0 409 Conflict");
+			$js_encode = json_encode(array('status'=>FALSE, 'message'=>'User creation failed'), true);
+			echo $js_encode;
+			break;
+		}
+
+		$userInfo = $user->createUser();
+
+		if(!empty($userInfo)) {
+			header("HTTP/1.0 200 OK");
+			$js_encode = json_encode(array('status'=>TRUE, 'message'=>'User created Successfully'), true);
+		} else {
+			header("HTTP/1.0 409 Conflict");
+			$js_encode = json_encode(array('status'=>FALSE, 'message'=>'User creation failed'), true);
+		}
+
+		header('Content-Type: application/json');
+		echo $js_encode;
+		break;
+	default:
+		header("HTTP/1.0 405 Method Not Allowed");
+		break;
+}
+?>
