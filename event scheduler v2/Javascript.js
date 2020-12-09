c3e390759c9bf8abca26d8a9e3eac1934757a36c
lfernandogalvis <61099417+lfernandogalvis@users.noreply.github.com>
2020-12-09 18:23:06 -0500

    javascript clean up

--- event scheduler v2/Javascript.js
+++ event scheduler v2/Javascript.js
@@ -1,10 +1,12 @@
-// var urlBase = 'http://knightacts.ueuo.com'; //http://vh1/api/event/eventsApproved.php
+// ***    Make sure to change the directory paths for 
+//        the javascript functions for running locally .  
+//		  URLs are spread around the code and it might 
+//        need to be replaced in more than one line.    ***
+
 var urlBase = '/api/event/';
 var extension = 'php';
-
 var userId = 0;
 var user = "";
-
 var usersJson = null;
 
 // Creates an account for the new user.
@@ -65,6 +67,7 @@ function doSignUp()
 	}
 }
 
+// gets all the events from an admin
 function getAdminEvents(){
 	document.getElementById("eventResult").innerHTML = "";
 	document.getElementById("eventsOrganized").innerHTML = "";
@@ -114,6 +117,8 @@ function getAdminEvents(){
 		console.log(err);
 	}
 }
+
+// gets events for users
 function getUserEvents(){
 	document.getElementById("userEvent").innerHTML = "";
 	document.getElementById("eventAttendance").innerHTML = "";
@@ -165,6 +170,7 @@ function getUserEvents(){
 
 
 }
+
 // Logs existing user into account.
 function doLogin()
 {
@@ -179,7 +185,7 @@ function doLogin()
 	document.getElementById("loginResult").innerHTML = "";
 
 	var jsonPayload = '{"username" : "' + login + '", "password" : "' + password + '"}';
-	var url = 'http://vh1/api/user/login.' + extension;
+	var url = 'http://vh1/api/user/login.' + extension; //needs to be modified 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, false);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
@@ -221,6 +227,7 @@ function doLogin()
 	}
 }
 
+// gets role of a user when logging in 
 function getRole()
 {
 	userId = "";
@@ -232,7 +239,7 @@ function getRole()
 	document.getElementById("loginResult").innerHTML = "";
 
 	var jsonPayload = '{"username" : "' + login + '"}';
-	var url = "http://vh1/api/user/getRole.php"
+	var url = "http://vh1/api/user/getRole.php" //needs to be modified
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, false);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
@@ -263,6 +270,7 @@ function getRole()
 
 }
 
+// saves cookies for login
 function saveCookie()
 {
 	var minutes = 20;
@@ -271,6 +279,7 @@ function saveCookie()
 	document.cookie = "userId=" + userId + ";expires=" + date.toGMTString();
 }
 
+// read cookies for login
 function readCookie()
 {
 	userId = -1;
@@ -296,390 +305,19 @@ function readCookie()
 	}
 }
 
-function doLogout()
-{
-	userId = 0;
-	document.cookie = "userId= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "index.html";
-}
-
+//returns to login
 function returnToLogin()
 {
 	window.location.href = "index.html";
 }
 
+//moves to signUp page
 function moveToSignUp()
 {
 	window.location.href = "signUp.html";
 }
 
-function returnToContactPage()
-{
-	window.location.href = "contactPage.html";
-	getAllContacts();
-}
-
-function getTitle()
-{
-    readCookie();
-    var url = urlBase + '/api/user/get/' + userId;
-
-    var xhr = new XMLHttpRequest();
-	xhr.open("GET", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function()
-		{
-		    json = JSON.parse( xhr.responseText );
-
-			if (this.readyState == 4 && this.status == 200)
-			{
-				document.getElementById("userNameTitle").innerHTML = json.Login + "'s  Knightacts";
-			}
-			else
-			{
-			    document.getElementById("userNameTitle").innerHTML = "My Knightacts";
-			}
-		};
-		xhr.send(null);
-	}
-	catch(err)
-	{
-		document.getElementById("userNameTitle").innerHTML = "My Knightacts";
-	}
-}
-
-function goToAddContact()
-{
-	window.location.href = "createNewContact.html";
-}
-
-// Creates a new contact
-function addContact()
-{
-	readCookie();
-
-	var firstName = document.getElementById("firstNameText").value;
-	var lastName = document.getElementById("lastNameText").value;
-	var emailContact = document.getElementById("emailContact").value;
-	var phoneNumber = document.getElementById("phoneNumber").value;
-	var addressContact = document.getElementById("addressContact").value;
-	var notesContact = document.getElementById("notesContact").value;
-	document.getElementById("contactAddResult").innerHTML = "";
-
-	if (firstName == '' || lastName == '')
-	{
-		document.getElementById("contactAddResult").innerHTML = "Please enter the required fields.";
-		return;
-	}
-
-
-	else
-	{
-		var jsonPayload = '{"FirstName" : "' + firstName + '", "LastName" : "' +lastName+ '", "Email" : "' +emailContact+ '", "PhoneNumber" : "' +phoneNumber+ '", "Address" : "' +addressContact+ '", "AdditionalNotes" : "' +notesContact+ '", "UserID" : ' + userId + '}';
-		var url = urlBase + '/api/contact/create.' + extension;
-
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", url, true);
-		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-		try
-		{
-			xhr.onreadystatechange = function()
-			{
-				if (this.readyState == 4 && this.status == 200)
-				{
-					document.getElementById("contactAddResult").innerHTML = "Knightact has been Added";
-
-					setTimeout(returnToContactPage, 1000);
-				}
-			};
-			xhr.send(jsonPayload);
-		}
-		catch(err)
-		{
-			document.getElementById("contactAddResult").innerHTML = err.message;
-		}
-	}
-}
-
-// SearchContacts2() is what is currently being called in contactPage.html. It does a "live" search
-// based on tag name. Needs to instead go off of firstName, lastName of the contact objects - which I don't think
-// are being stored. Also needs to be connected to the API.
-
-// TODO : Change to read from var usersJson - json object
-function searchContacts()
-{
-	var srch = document.getElementById("searchBar").value;
-	var contactList = "";
-	var input, filter, ul, li, a, i, txtValue;
-
-    if (srch == '')
-    {
-        hideAllContacts();
-        return;
-    }
-
-
-	// This block of code does a "live"
-    input = document.getElementById("searchBar");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if ((txtValue.toUpperCase().indexOf(filter) > -1) && (a.id != "message")) {
-            li[i].style.display = "";
-        } else {
-			li[i].style.display = "none";
-	 	}
-    }
-}
-
-function loadEditPage()
-{
-	var urlParams = new URLSearchParams(window.location.search);
-
-	var firstName = document.getElementById("firstNameText");
-	var lastName = document.getElementById("lastNameText");
-	var emailContact = document.getElementById("emailContact");
-	var phoneNumber = document.getElementById("phoneNumber");
-	var addressContact = document.getElementById("addressContact");
-	var notesContact = document.getElementById("notesContact");
-
-	var jsonPayload = null;
-	var url = urlBase + '/api/contact/get/' + Number(urlParams.get('id'));
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function()
-		{
-
-			if (this.readyState == 4 && this.status == 200)
-			{
-				var jsonObject = JSON.parse( xhr.responseText );
-
-				firstName.value = jsonObject.contact.FirstName;
-				lastName.value = jsonObject.contact.LastName;
-				emailContact.value = jsonObject.contact.Email;
-				phoneNumber.value = jsonObject.contact.PhoneNumber;
-				addressContact.value = jsonObject.contact.Address;
-				notesContact.value = jsonObject.contact.AdditionalNotes;
-
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("editResult").innerHTML = err.message;
-	}
-}
-
-function editPage()
-{
-    var urlParams = new URLSearchParams(window.location.search);
-
-	readCookie();
-
-	var firstName = document.getElementById("firstNameText").value;
-	var lastName = document.getElementById("lastNameText").value;
-	var emailContact = document.getElementById("emailContact").value;
-	var phoneNumber = document.getElementById("phoneNumber").value;
-	var addressContact = document.getElementById("addressContact").value;
-	var notesContact = document.getElementById("notesContact").value;
-
-	if (firstName == '' || lastName == '')
-	{
-		document.getElementById("contactEditResult").innerHTML = "Please enter the required fields.";
-		return;
-	}
-
-	else
-	{
-		//This will allow the change
-		var jsonPayload = '{"FirstName" : "' + firstName + '", "LastName" : "' +lastName+ '", "Email" : "' +emailContact+ '", "PhoneNumber" : "' +phoneNumber+ '", "Address" : "' +addressContact+ '", "AdditionalNotes" : "' +notesContact+ '", "ID" : ' + Number(urlParams.get('id')) + '}';
-		var url = urlBase + '/api/contact/update';
-
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", url, true);
-		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-		try
-		{
-			xhr.onreadystatechange = function()
-			{
-
-				if (this.readyState == 4 && this.status == 200)
-				{
-					document.getElementById("contactEditResult").innerHTML = "Knightact has been Updated";
-
-					setTimeout(returnToContactPage, 1000);
-
-				}
-			};
-			xhr.send(jsonPayload);
-		}
-		catch(err)
-		{
-			document.getElementById("contactEditResult").innerHTML = err.message;
-		}
-	}
-}
-
-
-function goToEditPage(id)
-{
-	window.location.href = "editContact.html?id=" + Number(id);
-}
-
-function deleteContact()
-{
-    var urlParams = new URLSearchParams(window.location.search);
-
-	readCookie();
-
-	var prompt = confirm("Are you sure you want to delete this Knightact?");
-	if(prompt)
-	{
-		var url = urlBase + '/api/contact/delete/' + Number(urlParams.get('id'));
-		var jsonPayload = null;
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", url, true);
-		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-		try
-		{
-		    xhr.onreadystatechange = function()
-			{
-
-				if (this.readyState == 4 && this.status == 200)
-				{
-
-				    window.location.href = "contactPage.html";
-				}
-			}
-			xhr.send(jsonPayload);
-
-		}
-		catch(err)
-		{
-			document.getElementById("contactEditResult").innerHTML = err.message;
-		}
-	}
-}
-
-function getAllContacts()
-{
-	readCookie();
-
-    getAllContactsUser(userId);
-}
-
-function getAllContactsUser(id)
-{
-	var url = urlBase + '/api/contact/getUserContacts/' + Number(id);
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url, false);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function()
-		{
-		    var div = document.getElementById("myUL");
-		    usersJson = JSON.parse( xhr.responseText );
-
-			if (usersJson.status)
-			{
-			    var content = document.createElement("a");
-				content.id = "message";
-				content.setAttribute("onclick", "showAllContacts();");
-
-				content.innerHTML = "Start a search to show your contacts<br>click me to show all of your contacts";
-
-				var contact = document.createElement("li");
-				contact.style.display = "";
-				contact.appendChild(content);
-
-				div.appendChild(contact);
-
-
-			    for (i = 0; i < usersJson.contacts.length; i++)
-			    {
-			        var content = document.createElement("a");
-			        content.id = "contact";
-			        content.setAttribute("onclick", "goToEditPage(" + usersJson.contacts[i].ID + ");");
-
-			        content.innerHTML = usersJson.contacts[i].FirstName + " " + usersJson.contacts[i].LastName;
-
-			        var contact = document.createElement("li");
-			        contact.style.display = "none";
-	                contact.appendChild(content);
-
-	                div.appendChild(contact);
-			    }
-			}
-			else
-			{
-				var content = document.createElement("a");
-				content.id = "message";
-
-				content.innerHTML = "No contacts found";
-
-				var contact = document.createElement("li");
-				contact.style.display = "";
-				contact.appendChild(content);
-
-				div.appendChild(contact);
-			}
-		}
-		xhr.send(null); // sending null since get request
-	}
-	catch(err)
-	{
-		//document.getElementById("editResult").innerHTML = err.message;
-		alert(err.message);
-	}
-}
-
-function showAllContacts()
-{
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        var a = li[i].getElementsByTagName("a")[0];
-        if (a.id != "message")
-        {
-            li[i].style.display = "";
-        }
-        else
-        {
-            li[i].style.display = "none";
-        }
-    }
-}
-
-function hideAllContacts()
-{
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        var a = li[i].getElementsByTagName("a")[0];
-        if (a.id == "message")
-        {
-            li[i].style.display = "";
-        }
-        else
-        {
-            li[i].style.display = "none";
-        }
-    }
-}
-
-// Adding searchBox as BOOL for focusing lists only in main box
+// displays all the events. adding searchBox as BOOL for focusing lists only in main box
 function renderEvents(titles, descriptions, urls, dates, endDates, addresses, citys, eventIds, button, searchBox){
 
 	if (descriptions == null){
@@ -776,16 +414,18 @@ function renderEvents(titles, descriptions, urls, dates, endDates, addresses, ci
 	return list;
 }
 
+// test function 
 function createEventt(){
 	alert("Hello World!");
 }
 
+// function for enrolling events
 function enroll(title, description, url, startdate, enddate, address, city, event_id){
 
 	var username = sessionStorage.getItem('userId');
 	// var jsonPayload = {"username": username, "eventID": eventID.toString()};
 	var jsonPayload = '{"username" : "' + username + '", "eventID" : "' + event_id + '"}';
-	var url = "http://vh1/api/enrollment/enroll.php";
+	var url = "http://vh1/api/enrollment/enroll.php"; //needs to be changed
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, false);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
@@ -810,6 +450,7 @@ function enroll(title, description, url, startdate, enddate, address, city, even
 	}
 }
 
+// sorts events by city
 function sortByCity() {
 	filter = document.getElementById('fname').value;
 	var event_list = document.querySelectorAll('.event-search');
@@ -830,6 +471,7 @@ function sortByCity() {
 	document.getElementById('fname').value = "";
 }
 
+// sorts events by date
 function sortByDate() {
 	sDate = document.getElementById('sDate').value;
 	eDate = document.getElementById('eDate').value;
@@ -864,6 +506,7 @@ function sortByDate() {
 	document.getElementById('eDate').value = "";
 }
 
+// shows all the events
 function showAllEvents() {
 	var event_list = document.querySelectorAll('.event-search');
 	[].slice.call(event_list).forEach(function(temp) {
@@ -871,6 +514,7 @@ function showAllEvents() {
 	});
 }
 
+// function that renders everything in the main dashboards. 
 window.onload = function() {
 
 	if(window.location.pathname == "/dashboard.html"){
@@ -1007,6 +651,7 @@ window.onload = function() {
 
 }
 
+// displays the form for creating aan event
 function displayCreateForm(){
 
 	if(document.getElementById("creationArea").style.display == 'flex')
@@ -1016,6 +661,7 @@ function displayCreateForm(){
 		document.getElementById("creationArea").style.display = 'flex';
 }
 
+// takes user input to create events
 function createEvent(){
 
 	var title = document.getElementById("etitle").value;
@@ -1029,7 +675,7 @@ function createEvent(){
 	var jsonPayload = '{"title" : "' + title + '", "description" : "' + description +  '", "url" : "' + url +  '", "startdate" : "' + startdate +  '", "enddate" : "' + enddate +  '", "address" : "' + address +  '", "username" : "' + sessionStorage.getItem('userId') +  '", "city" : "' + city +  '"}';
 
 
-	var url = "http://vh1/api/event/create.php";
+	var url = "http://vh1/api/event/create.php"; //needs to be changed
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, false);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
